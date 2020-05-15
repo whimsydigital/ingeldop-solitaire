8eef7535fdfa00baa0b2291a7565e2885c6015fb
Bryce Kellogg <bryce@kellogg.org>
2020-05-14 19:47:36 -0700

    Cleaned up various button actions

--- app/src/main/java/com/brycekellogg/ingeldop/IngeldopActivity.java
+++ app/src/main/java/com/brycekellogg/ingeldop/IngeldopActivity.java
@@ -3,18 +3,10 @@ package com.brycekellogg.ingeldop;
 
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.menu.ActionMenuItemView;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
 
 /* Main activity for Ingeldop. The game is a single activity app, where all interaction
  * is done in the main activity. We have some popups and dialogs for things such as
@@ -29,15 +21,15 @@ public class IngeldopActivity extends AppCompatActivity implements View.OnClickL
     private int marginBetweenMin;
     private int marginBetweenMax;
 
-    private float disableAlpha;
-    private float enableAlpha;
-
-
     // UI Elements
-    View dealButton;
+    HorizontalScrollView scrollView;
+    DealButton dealButton;
+    View discardButton;
     View zoomOutButton;
     View zoomInButton;
+    View handView;
 
+    // Layout params used for zooming
     ConstraintLayout.LayoutParams dealButtonLayoutParams;
 
 
@@ -54,19 +46,18 @@ public class IngeldopActivity extends AppCompatActivity implements View.OnClickL
         findViewById(R.id.statsButton).setOnClickListener(this);
         findViewById(R.id.settingsButton).setOnClickListener(this);
 
-
         marginBetween     = 100;  // TODO: loaded from saved state
         marginBetweenStep = 30;   // TODO: based on screen size
         marginBetweenMin  = 10;   // TODO: based on screen size
         marginBetweenMax  = 400;  // TODO: based on screen size
 
-        disableAlpha = Float.parseFloat(getString(R.string.disableAlpha));
-        enableAlpha  = Float.parseFloat(getString(R.string.enableAlpha));
-
         // Save UI elements we interact with
-        dealButton = findViewById(R.id.dealButton);
+        scrollView    = findViewById(R.id.scrollView);
+        dealButton    = findViewById(R.id.dealButton);
+        discardButton = findViewById(R.id.discardButton);
         zoomOutButton = findViewById(R.id.zoomOutButton);
         zoomInButton  = findViewById(R.id.zoomInButton);
+        handView      = findViewById(R.id.handView);
 
         // Layout params for zooming
         dealButtonLayoutParams = (ConstraintLayout.LayoutParams) dealButton.getLayoutParams();
@@ -76,86 +67,83 @@ public class IngeldopActivity extends AppCompatActivity implements View.OnClickL
         doZoom(false, false);  // Update scale
 
 //        restoreState();          // Try to load saved state
-        update();                // Update layout/graphics
-    }
-
-
-    /* Display an alert with the given message. This currently
-     * makes use of the Toast functionality. Any alerts needed
-     * by any component should make use of this function.  */
-    public void alert(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-
-    /* Update the graphical state of the play area. This involves
-     * updating the graphical (empty/enabled) state of the deal
-     * button, requesting a layout and invalidating the various
-     * views, and potentially displaying an alert if the game
-     * is over. This happens on every deal and discard, which
-     * might cause some overhead.  */
-    public void update() {
-        // Update deal button state on empty or game over
-        DealButton dealButton = (DealButton) findViewById(R.id.dealButton);
-        dealButton.setEmpty(game.deckSize() == 0);
-        dealButton.setEnabled(!game.gameOver());
-
-        // Update layouts and graphics
-        findViewById(R.id.layout).requestLayout();
-        findViewById(R.id.dealButton).requestLayout();
-        findViewById(R.id.handView).requestLayout();
-
-        // Show alerts if needed
-        if (game.gameOver()) alert(getString(R.string.gameOverText));
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.dealButton:    deal();    break;
-            case R.id.discardButton: discard(); break;
-            case R.id.newGameButton: newGame(); break;
-            case R.id.statsButton:    break;
-            case R.id.settingsButton: break;
+            case R.id.dealButton:    doDeal();    break;
+            case R.id.discardButton: doDiscard(); break;
+            case R.id.newGameButton: newGame();   break;
             case R.id.zoomInButton:  doZoom(true, false); break;
             case R.id.zoomOutButton: doZoom(false, true); break;
-
+            case R.id.statsButton:    break;
+            case R.id.settingsButton: break;
         }
     }
 
-    /* Processes a dealButton onClick action. We deal a card and update the button
-     * state based on if the deck is empty or if the game is over. If the deck is
-     * empty, we set the dealButton's empty state. If the game is over, we set
-     * the dealButtons enabled state to false. Finally, we notify the context. */
-    public void deal() {
-        // Do the deal
-        game.deal();
 
-        // Scroll to the right on deal
-        ((HorizontalScrollView) findViewById(R.id.scrollView)).fullScroll(View.FOCUS_RIGHT);
-
-        // Update activity
-        update();
+    /**
+     * Processes a deal action and update UI elements.
+     *
+     * We deal a card, request a layout update for the hand view to deal
+     * with the change in the hand size, and instruct the scroll view to
+     * scroll all the way to the right so the most recent dealt card is
+     * displayed. If the deck is empty, we update the deal button state
+     * so it can show an empty image. If the game is over, we call the
+     * game over logic.  */
+    private void doDeal() {
+        game.deal();                                // Do the deal
+        handView.requestLayout();                   // Update hand
+        scrollView.fullScroll(View.FOCUS_RIGHT);    // Scroll to the right on deal
+        dealButton.setEmpty(game.deckSize() == 0);  // Update button state if empty
+        if (game.gameOver()) doGameOver();          // Handle game over
     }
 
-    /* Process a discard action. We attempt to discard the selected cards, but if a discard
-     * is not allowed due to currently selected cards, an error pop-up is displayed. If a
-     * discard is allowed, the selected cards are discarded, we potentially update the
-     * deal button image if the game is over, and request a redraw of the hand view. If
-     * the game is over, we additionally display a popup notification. */
-    public void discard() {
+
+    /**
+     * Process a discard action and handle incorrect discards.
+     *
+     * We attempt to discard the selected cards, but if a discard is
+     * not allowed due to currently selected cards we catch the thrown
+     * exception and display a pop-up describing the discard exception. If a
+     * discard is allowed, the selected cards are discarded and we update the
+     * layout of the hand (because the size changes with a discard). If the
+     * game is over, we call the game over logic.
+     *
+     * TODO: catch different exception types for different discard fails  */
+    private void doDiscard() {
         try {
-            // Do the discard
-            game.discard();
-
-            // Update activity
-            update();
-
+            game.discard();                     // Do the discard
+            handView.requestLayout();           // Update hand
+            if (game.gameOver()) doGameOver();  // Handle game over
         } catch (DiscardException e) {
-            // Update activity with error
-            alert(e.getMessage());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    /**
+     * Start a new game and reset deck/hand views.
+     *
+     * A new game is initiated by assigning a new Ingeldop
+     * instance to this.game, resetting the state of the
+     * deal button, and requesting a layout update of the
+     * hand view. We request a layout update instead of just
+     * a redraw because clearing the hand will change the size
+     * and force the containing scroll view to remove scroll bars. */
+    private void newGame() {
+        // Start a new game
+        this.game = new Ingeldop();
+
+        // Reset deal & discard button state
+        dealButton.setEnabled(true);
+        dealButton.setEmpty(false);
+        discardButton.setEnabled(true);
+
+        // Redraw the hand view
+        handView.requestLayout();
     }
 
 
@@ -169,7 +157,7 @@ public class IngeldopActivity extends AppCompatActivity implements View.OnClickL
      * step. This forces the constraint layout to adjust the sizes of
      * the UI elements to fit within the margins. We also enable or
      * disable the zoom buttons based on if we have reached zoom limits. */
-    public void doZoom(boolean in, boolean out) {
+    private void doZoom(boolean in, boolean out) {
         // Update margins if zooming in/out
         if (in)  marginBetween -= marginBetweenStep;
         if (out) marginBetween += marginBetweenStep;
@@ -186,19 +174,24 @@ public class IngeldopActivity extends AppCompatActivity implements View.OnClickL
     }
 
 
-    /* Starts a new game, resets deal button image to full
-     * deck, and requests a graphics/layout update.  */
-    void newGame() {
-        // Start a new game
-        this.game = new Ingeldop();
+    /**
+     * Update the UI elements when the game is over.
+     *
+     * If the game is over we disable the deal and discard buttons and
+     * display a message to the player about the number of cards left
+     * in the hand (or a win message of none left).
+     *
+     * TODO: save game statistics  */
+    private void doGameOver() {
+        // Disable deal & discard buttons
+        dealButton.setEnabled(false);
+        discardButton.setEnabled(false);
 
-        // Reset deal button state
-        DealButton dealButton = (DealButton) findViewById(R.id.dealButton);
-        dealButton.setEmpty(false);
-        dealButton.setEnabled(true);
-
-        // Redraw the hand view
-        update();
+        // Show game over message
+        String msg;
+        if (game.handSize() == 0) msg = getString(R.string.gameWinText);
+        else                      msg = getString(R.string.gameOverText, game.handSize());
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
 
